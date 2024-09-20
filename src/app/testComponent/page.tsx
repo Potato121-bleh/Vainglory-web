4a01f797dc6134374b3585ca6bbcc96a7273dba6
Ly Sochetra <lysochetra95@gmail.com>
2024-09-20 10:07:19 +0700

    merge

--- src/app/testComponent/page.tsx
+++ src/app/testComponent/page.tsx
@@ -1,24 +1,78 @@
 'use client'
 
-import { Button, Modal } from '@mui/material'
+import { Box, Button, Modal } from '@mui/material'
 import { useState } from 'react'
 
 export default function testComponent() {
     let [openModal, setOpenModal] = useState<boolean>(false)
-    const handleClick = () => {
+    let [selectedHero, setSelectedHero] = useState<heroArrType[]>([])
+
+    const handleClick = (elementId: number) => {
+        for (let i = 0; i < heroArr.length; i++) {
+            if (heroArr[i].id == elementId) {
+                let selectedHeroTemp = [heroArr[i]]
+                setSelectedHero(selectedHeroTemp)
+            }
+        }
         setOpenModal(true)
     }
+    const handleClose = () => {
+        setOpenModal(false)
+    }
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    }
+
+    type heroArrType = {
+        id: number
+        name: string
+    }
+
+    const heroArr: heroArrType[] = [
+        {
+            id: 1,
+            name: 'Krul',
+        },
+        {
+            id: 2,
+            name: 'Malane',
+        },
+        {
+            id: 3,
+            name: 'Ringo',
+        },
+    ]
+
     return (
         <>
-            <Button variant="contained">Contained</Button>
-            <Modal
-                open={openModal}
-                //onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-            >
-                <h1>Hello brother</h1>
-            </Modal>
+            {heroArr.map((element) => (
+                <Button
+                    key={element.id}
+                    onClick={() => handleClick(element.id)}
+                    variant="contained"
+                >
+                    {element.name}
+                </Button>
+            ))}
+            {selectedHero.map((element) => (
+                <Modal
+                    open={openModal}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>Hero: {element.name}</Box>
+                </Modal>
+            ))}
         </>
     )
 }
