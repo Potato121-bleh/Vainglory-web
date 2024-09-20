fe2e039edf23a22001285418aad71a53e592535e
Ly Sochetra <lysochetra95@gmail.com>
2024-09-20 08:10:25 +0700

    first commit

---
+++ src/app/testComponent/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Button, Modal } from '@mui/material'
+import { useState } from 'react'
+
+export default function testComponent() {
+    let [openModal, setOpenModal] = useState<boolean>(false)
+    const handleClick = () => {
+        setOpenModal(true)
+    }
+    return (
+        <>
+            <Button variant="contained">Contained</Button>
+            <Modal
+                open={openModal}
+                //onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <h1>Hello brother</h1>
+            </Modal>
+        </>
+    )
+}
