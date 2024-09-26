5983377e41cb8433f4e66d730e8369b8de437254
Ly Sochetra <lysochetra95@gmail.com>
2024-09-26 10:02:07 +0700

    fix lorem

--- src/app/Component/footer/footer.tsx
+++ src/app/Component/footer/footer.tsx
@@ -1,96 +1,104 @@
-import Image from "next/image";
-import React from "react";
-import "./footer.css";
+import Image from 'next/image'
+import React from 'react'
+import './footer.css'
 
 const footer = () => {
-  return (
-    <div className="footer-con">
-      <div className="list-logo-con">
-        <div className="list-logo">
-          <Image
-            src="/footer/html.png"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo">
-          <Image
-            src="/footer/css.png"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo">
-          <Image
-            src="/footer/js.jpg"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo">
-          <Image
-            src="/footer/tailwind.png"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo">
-          <Image
-            src="/footer/Ts.jpg"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo ">
-          <Image
-            style={{ width: 50, height: 40, objectFit: "cover" }}
-            src="/footer/nextJsN.png"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-        <div className="list-logo">
-          <Image
-            src="/footer/mui.png"
-            width={100}
-            height={100}
-            alt=""
-            className="pic"
-          />
-        </div>
-      </div>
+    return (
+        <div className="footer-con">
+            <div className="list-logo-con">
+                <div className="list-logo">
+                    <Image
+                        src="/footer/html.png"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo">
+                    <Image
+                        src="/footer/css.png"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo">
+                    <Image
+                        src="/footer/js.jpg"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo">
+                    <Image
+                        src="/footer/tailwind.png"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo">
+                    <Image
+                        src="/footer/Ts.jpg"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo ">
+                    <Image
+                        style={{ width: 50, height: 40, objectFit: 'cover' }}
+                        src="/footer/nextJsN.png"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+                <div className="list-logo">
+                    <Image
+                        src="/footer/mui.png"
+                        width={100}
+                        height={100}
+                        alt=""
+                        className="pic"
+                    />
+                </div>
+            </div>
 
-      <p className="text">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis facere
-        nihil fugiat. hjkhgagjkjkdjk
-        <span className="second-line">
-          quam sit ullam placeat hic mollitia numquam soluta eaque doloribus
-        </span>
-      </p>
+            <div
+                className="text"
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <p>
+                    Water in every direction is a terrifying luxury. <br />
+                    Truth goes unarmed, but I shall carry a spear
+                </p>
+                <p>By </p>
+                <b style={{ marginTop: '15px', fontSize: '17px' }}>Idris</b>
+            </div>
 
-      <div className="footer-logo">
-        <Image
-          src="/image/logo/logoM.png"
-          width={100}
-          height={100}
-          alt="logo"
-          className="lg"
-        />
-      </div>
-    </div>
-  );
-};
+            <div className="footer-logo">
+                <Image
+                    src="/image/logo/logoM.png"
+                    width={100}
+                    height={100}
+                    alt="logo"
+                    className="lg"
+                />
+            </div>
+        </div>
+    )
+}
 
-export default footer;
+export default footer
